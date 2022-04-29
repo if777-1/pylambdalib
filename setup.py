29567ec877e28d21e4af333d68bc9a3a4e2a63f7
thcabrera <thiagomartincabreralavezzi@gmail.com>
2022-04-29 16:16:42 -0300

    Modificacion en el setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 setup(
-name='pylambdalib',
+name='lambdapylib',
 version='1.0.0',
 description='Python package for Lambda Solution operations',
 url='#',
