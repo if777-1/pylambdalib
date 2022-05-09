be6a3ccbe1759913d821470414222cc0f2601d15
thcabrera <thiagomartincabreralavezzi@gmail.com>
2022-05-09 10:38:02 -0300

    Paquetes agregados a requerimientos

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages
 from setuptools import setup
 setup(
 name='pylambdalib',
-version='1.2.4.3',
+version='1.2.4.4',
 description='Python package for Lambda Solution operations',
 url='https://github.com/thcabrera/pylambdalib',
 author='Thiago Cabrera Lavezzi',
 author_email='thiagomartincabreralavezzi@gmail.com',
 license='Lambda Solution',
-install_requires=['shapely'],
+install_requires=['shapely','scipy','redis','pyshp','pyautocad','zipfile','simplekml','pyrebase'],
 packages=find_packages(),
 zip_safe=False
 )
