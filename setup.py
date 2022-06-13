1b86b9dba047e12a5545e8cfd553dc8f4e01b852
Thiago Cabrera Lavezzi <62122863+thcabrera@users.noreply.github.com>
2022-06-13 11:56:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if platform.system() == "Linux":
 
 setup(
 name='pylambdalib',
-version='1.2.4.4',
+version='1.2.4.5',
 description='Python package for Lambda Solution operations',
 url='https://github.com/thcabrera/pylambdalib',
 author='Thiago Cabrera Lavezzi',
